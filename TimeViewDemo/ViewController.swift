40fec9aed0aa4054ebd52afc55cce7cc3b674879
Ata Doruk <ata.doruk@ekol.com>
2018-02-27 12:33:51 +0300

    Initial Commit

---
+++ TimeViewDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TimeViewDemo
+//
+//  Created by Ata Doruk on 27.02.2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
